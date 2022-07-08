e9954044ed932f7d3e9b9635397d051fd9133ade
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-07-08 20:03:57 +0200

    modified init and build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,18 +8,29 @@ MUSL_VERSION=1.1.16
 
 
 ARCH_VAR=$(uname -m)
-TARGET_VAR=$ARCH_VAR-pc-linux-gnu
+TARGET_VAR=$ARCH_VAR-unknown-linux-gnu
+HOST_VAR=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
 
+CC=gcc
 
 mkdir -p rootfs
 mkdir -p staging
 mkdir -p iso/boot
+mkdir -p iso/efi/boot
+mkdir -p iso/boot/grub/i386-pc
+mkdir -p rootfs/tools
+mkdir -p rootfs/cross-tools
+
+
 
 SOURCE_DIR=$PWD
 AIROOTFS=$SOURCE_DIR/airootfs
 ROOTFS=$SOURCE_DIR/rootfs
 STAGING=$SOURCE_DIR/staging
 ISO_DIR=$SOURCE_DIR/iso
+
+export PATH=$PATH:$ROOTFS/cross-tools/bin
+
 
 mkdir -p $ROOTFS/{bin,boot,dev,sys,home,mnt,proc,run,tmp,var,etc,sbin,lib/{firmware,modules}} $ROOTFS/usr/{,local/}{bin,include,lib,sbin,share/{color,dict,doc,info,locale,man,misc,terminfo,zoneinfo}} $ROOTFS/var/{cache,lib,local,lock,opt,run,spool,empty,log}
 touch $ROOTFS/var/log/{btmp,lastlog,faillog,wtmp}
@@ -37,22 +48,207 @@ mkdir -pv $ROOTFS/etc/network/if-{post-{up,down},pre-{up,down},up,down}.d
 mkdir -pv $ROOTFS/usr/share/udhcpc
 chmod +x $ROOTFS/usr/share/udhcpc/default.script
 ln -sv $ROOTFS/proc/self/mounts $ROOTFS/etc/mtab
-mknod -m 600 $LFS/dev/console c 5 1
-mknod -m 666 $LFS/dev/null c 1 3
-
+mknod -m 600 $ROOTFS/dev/console c 5 1
+mknod -m 666 $ROOTFS/dev/null c 1 3
+mknod -m 600 $ROOTFS/lib/udev/devices/console c 5 1
+mknod -m 666 $ROOTFS/lib/udev/devices/null c 1 3
 
 
 
 cp -v -r $AIROOTFS/* $ROOTFS 
 
+# CROSS COMPILATION TOOLS
+cd $STAGING
+
+wget -nc -O bc.tar.bz2 http://alpha.gnu.org/gnu/bc/bc-1.06.95.tar.bz2
+wget -nc -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
+wget -nc -O file.tar.gz ftp://ftp.astron.com/pub/file/file-5.15.tar.gz
+wget -nc -O m4.tar.xz http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.xz
+wget -nc -O ncurses.tar.gz ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
+wget -nc -O ncurses-bash.patch http://patches.cross-lfs.org/2.1.0/ncurses-5.9-bash_fix-1.patch
+wget -nc -O gmp.tar.xz http://ftp.gnu.org/gnu/gmp/gmp-5.1.3.tar.xz
+wget -nc -O mpfr.tar.xz http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.xz
+wget -nc -O mpc.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.0.1.tar.gz
+wget -nc -O cloog.tar.gz http://www.bastoul.net/cloog/pages/download/cloog-0.18.0.tar.gz
+wget -nc -O binutils.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2
+wget -nc -O gcc.tar.bz2 ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.8.1/gcc-4.8.1.tar.bz2
+wget -nc -O gcc-branch_update.patch http://patches.cross-lfs.org/2.1.0/gcc-4.8.1-branch_update-3.patch
+wget -nc -O gcc-pure64_specs.patch http://patches.cross-lfs.org/2.1.0/gcc-4.8.1-pure64_specs-1.patch
+wget -nc -O eglibc.tar.xz https://launchpad.net/debian/+archive/primary/+sourcefiles/eglibc/2.18-2/eglibc_2.18.orig.tar.xz
+
+tar -xvf bc.tar.bz2
+tar -xvf kernel.tar.xz
+tar -xvf file.tar.gz
+tar -xvf m4.tar.xz
+tar -xvf ncurses.tar.gz
+tar -xvf gmp.tar.xz
+tar -xvf mpfr.tar.xz
+tar -xvf mpc.tar.gz
+tar -xvf cloog.tar.gz
+tar -xvf binutils.tar.bz2
+tar -xvf gcc.tar.bz2
+tar -xvf eglibc.tar.xz
+
+
+set -ex
 
 cd $STAGING
-wget -nc -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz
+cd bc-1.06.95
+./configure --prefix=$ROOTFS/cross-tools
+make -j$(nproc)
+make install -j$(nproc)
+
+
+cd $STAGING
+cd linux-${KERNEL_VERSION}
+make mrproper
+make ARCH=x86_64 INSTALL_HDR_PATH=$ROOTFS/tools headers_install
+
+
+cd $STAGING
+cd file-5.15
+./configure --prefix=$ROOTFS/cross-tools --disable-static
+make -j$(nproc)
+make install -j$(nproc)
+
+: '
+cd $STAGING
+cd m4-1.4.17
+./configure --prefix=$ROOTFS/cross-tools
+make -j$(nproc)
+make install -j$(nproc)
+'
+
+: '
+cd $STAGING
+cd ncurses-5.9
+patch -Np1 -i ../ncurses-bash.patch
+./configure --prefix=$ROOTFS/cross-tools --without-debug --without-shared
+make -C include -j$(nproc)
+make -C progs tic -j$(nproc)
+install -v -m755 progs/tic /cross-tools/bin
+
+
+cd $STAGING
+cd gmp-5.1.3
+./configure --prefix=$ROOTFS/cross-tools --enable-cxx --disable-static
+make -j$(nproc)
+make install -j$(nproc)
+
+cd $STAGING
+cd mpfr-3.1.2
+LDFLAGS="-Wl,-rpath,$ROOTFS/cross-tools/lib" ./configure --prefix=$ROOTFS/cross-tools --disable-static --with-gmp=$ROOTFS/cross-tools
+make -j$(nproc)
+make install -j$(nproc)
+
+cd $STAGING
+cd mpc-1.0.1
+LDFLAGS="-Wl,-rpath,$ROOTFS/cross-tools/lib" ./configure --prefix=$ROOTFS/cross-tools --disable-static --with-gmp=$ROOTFS/cross-tools --with-mpfr=$ROOTFS/cross-tools
+make -j$(nproc)
+make install -j$(nproc)
+
+cd $STAGING
+cd cloog-0.18.0
+LDFLAGS="-Wl,-rpath,$ROOTFS/cross-tools/lib" . ./configure --prefix=$ROOTFS/cross-tools --disable-static --with-gmp-prefix=$ROOTFS/cross-tools
+make -j$(nproc)
+make install -j$(nproc)
+
+cd $STAGING
+cd binutils-2.23.2
+sed -i -e 's/@colophon/@@colophon/' -e 's/~doc at cygnus.com/doc@@cygnus.com/' bfd/doc/bfd.texinfo
+mkdir -v ../binutils-build
+cd ../binutils-build
+AR=ar AS=as ../binutils-2.23.2/configure \
+  --prefix=$ROOTFS/cross-tools --host=${HOST_VAR} --target=${TARGET_VAR} \
+  --with-sysroot=${ROOTFS} --with-lib-path=$ROOTFS/tools/lib --disable-nls \
+  --disable-static --enable-64-bit-bfd --disable-multilib
+make configure-host
+make
+make install
+cp -v ../binutils-2.23.2/include/libiberty.h $ROOTFS/tools/include
+
+cd $STAGING
+cd gcc-4.8.1
+patch -Np1 -i ../gcc-branch_update.patch
+patch -Np1 -i ../gcc-pure64_specs.patch
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
+touch $ROOTFS/tools/include/limits.h
+mkdir -v ../gcc-build
+cd ../gcc-build
+AR=ar LDFLAGS="-Wl,-rpath,$ROOTFS/cross-tools/lib" \
+  ../gcc-4.8.1/configure --prefix=$ROOTFS/cross-tools \
+  --build=${HOST_VAR} --host=${HOST_VAR} --target=${TARGET_VAR} \
+  --with-sysroot=${ROOTFS} --with-local-prefix=$ROOTFS/tools \
+  --with-native-system-header-dir=$ROOTFS/tools/include --disable-nls \
+  --with-isl=$ROOTFS/cross-tools --with-cloog=$ROOTFS/cross-tools --with-mpc=$ROOTFS/cross-tools \
+  --without-headers --with-newlib --disable-decimal-float --disable-libgomp \
+  --disable-libmudflap --disable-libssp --disable-threads --disable-multilib \
+  --disable-libatomic --disable-libitm --disable-libsanitizer \
+  --disable-libquadmath --disable-target-libiberty --disable-target-zlib \
+  --with-system-zlib --enable-cloog-backend=isl --disable-isl-version-check \
+  --enable-languages=c --enable-checking=release
+make all-gcc all-target-libgcc -j$(nproc)
+make install-gcc install-target-libgcc -j$(nproc)
+
+cd $STAGING
+cd eglibc-2.18
+mkdir -v ../eglibc-build
+cd ../eglibc-builds
+echo "libc_cv_ssp=no" > config.cache
+BUILD_CC="gcc" CC="${TARGET_VAR}-gcc -m 64" \
+      AR="${TARGET_VAR}-ar" RANLIB="${TARGET_VAR}-ranlib" \
+      ../eglibc-2.18/configure --prefix=$ROOTFS/tools \
+      --host=${TARGET_VAR} --build=${HOST_VAR} \
+      --disable-profile --with-tls --enable-kernel=2.6.32 --with-__thread \
+      --with-binutils=$ROOTFS/cross-tools/bin --with-headers=$ROOTFS/tools/include \
+      --enable-obsolete-rpc --cache-file=config.cache
+
+make -j$(nproc)
+make install -j$(nproc)
+mv -v /tools/include/gnu/stubs{-64,}.h
+
+rm -rf $STAGING/gcc-4.8.1
+rm -rf $STAGING/gcc-build
+tar -xvf gcc.tar.bz2
+patch -Np1 -i ../gcc-branch_update.patch
+patch -Np1 -i ../gcc-pure64_specs.patch
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
+mkdir -v ../gcc-build
+cd ../gcc-build
+AR=ar LDFLAGS="-Wl,-rpath,$ROOTFS/cross-tools/lib" \
+  ../gcc-4.8.1/configure --prefix=$ROOTFS/cross-tools \
+  --build=${HOST_VAR} --target=${TARGET_VAR} --host=${HOST_VAR} \
+  --with-sysroot=${ROOTFS} --with-local-prefix=$ROOTFS/tools \
+  --with-native-system-header-dir=$ROOTFS/tools/include --disable-nls \
+  --with-sysroot=$ROOTFS --with-local-prefix=$ROOTFS/tools --disable-nls \
+  --enable-shared --disable-static --enable-languages=c,c++ \
+  --enable-__cxa_atexit --enable-c99 --enable-long-long --enable-threads=posix \
+  --disable-multilib --with-mpc=$ROOTFS/cross-tools --with-mpfr=$ROOTFS/cross-tools \
+  --with-gmp=$ROOTFS/cross-tools --with-cloog=$ROOTFS/cross-tools \
+  --enable-cloog-backend=isl --with-isl=$ROOTFS/cross-tools \
+  --disable-isl-version-check --with-system-zlib --enable-checking=release \
+  --enable-libstdcxx-time
+make AS_FOR_TARGET="${TARGET_VAR}-as" \
+    LD_FOR_TARGET="${TARGET_VAR}-ld"
+make install
+
+'
+
+set +ex
+
+
+
+
+
+cd $STAGING
 #wget -nc -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_VERSION}.tar.xz
 #wget -nc -O binutils.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.bz2
 wget -nc -O busybox.tar.bz2 http://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2
 wget -nc -O iana-etc.tar.bz2 http://sethwklein.net/iana-etc-${IANA_ETC_VERSION}.tar.bz2
 wget -nc -O iana-etc-patch.patch  http://patches.clfs.org/embedded-dev/iana-etc-${IANA_ETC_VERSION}-update-2.patch
+git clone https://github.com/memtest86plus/memtest86plus.git memtest86
 #wget -nc -O gcc.tar.bz2  http://gcc.gnu.org/pub/gcc/releases/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.bz2
 #wget -nc -O musl.tar.gz http://www.musl-libc.org/releases/musl-${MUSL_VERSION}.tar.gz
 #wget -nc -O mpfr.tar.bz2 http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.4.tar.bz2
@@ -60,176 +256,26 @@ wget -nc -O iana-etc-patch.patch  http://patches.clfs.org/embedded-dev/iana-etc-
 #wget -nc -O gmp.tar.bz2 http://ftp.gnu.org/gnu/gmp/gmp-6.1.1.tar.bz2
 
 
-tar -xvf kernel.tar.xz
+
+
+tar -xvf bc.tar.bz2
 tar -xvf busybox.tar.bz2
 #tar -xvf binutils.tar.bz2
 tar -xvf iana-etc.tar.bz2
 #tar -xvf gcc.tar.bz2
 #tar -xvf musl.tar.gz
 
-mkdir -p $ROOTFS/cross-tools/$TARGET_VAR
-ln -sfv $ROOTFS/cross-tools/$TARGET_VAR $ROOTFS/cross-tools/$TARGET_VAR/usr
-
 
 
 
 set -ex
 
-# CROSS TOOLCHAIN
-: '
+
+
 cd $STAGING
-cd binutils-$BINUTILS_VERSION
-sed '6009s/$add_dir//' -i ltmain.sh
-mkdir -v ../binutils-build
-cd ../binutils-build
-../binutils-2.27/configure \
-   --prefix=$ROOTFS/cross-tools \
-   --target=${TARGET_VAR} \
-   --with-sysroot=$ROOTFS \
-   --disable-nls \
-   --disable-werror
-make configure-host -j$(nproc)
+cd memtest86/build64
 make -j$(nproc)
-make install -j$(nproc)
-'
-: '
-cd $STAGING
-cd gcc-${GCC_VERSION}
-tar xf ../mpfr.tar.bz2
-mv -v mpfr-3.1.4 mpfr
-tar xf ../gmp.tar.bz2
-mv -v gmp-6.1.1 gmp
-tar xf ../mpc.tar.gz
-mv -v mpc-1.0.3 mpc
-if [$ARCH_VAR == x86_64]; then
-    sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
-fi
-'
-: '
-mkdir -v build
-cd build
-../configure                  \
-    --target=$TARGET_VAR        \
-    --prefix=${ROOTFS}/cross-tools       \
-    --with-glibc-version=2.35 \
-    --with-sysroot=$LFS       \
-    --with-newlib             \
-    --without-headers         \
-    --enable-initfini-array   \
-    --disable-nls             \
-    --disable-shared          \
-    --disable-multilib        \
-    --disable-decimal-float   \
-    --disable-threads         \
-    --disable-libatomic       \
-    --disable-libgomp         \
-    --disable-libquadmath     \
-    --disable-libssp          \
-    --disable-libvtv          \
-    --disable-libstdcxx       \
-    --enable-languages=c,c++
-make -j$(nproc)
-make install -j$(nproc)
-
-cd ..
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > `dirname $($TARGET_VAR-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
-
-cd $STAGING
-cd linux-${KERNEL_VERSION}
-make mrproper -j$(nproc)
-make headers_check -j$(nproc)
-make INSTALL_HDR_PATH=$ROOTFS/cross-tools/$TARGET_VAR headers_install -j$(nproc)
-
-cd $STAGING
-cd gcc-${GCC_VERSION}
-rm -rf build
-mkdir -v build
-cd       build
-../libstdc++-v3/configure           \
-    --host=$TARGET_VAR                 \
-    --build=$(../config.guess)      \
-    --prefix=/usr                   \
-    --disable-multilib              \
-    --disable-nls                   \
-    --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=${ROOTFS}/cross-tools/include/c++/11.2.0
-make -j$(nproc)
-make DESTDIR=$ROOTFS install
-
-cd $STAGING
-cd musl-$MUSL_VERSION
-
-CC=gcc  ./configure \
-  CROSS_COMPILE=${TARGET_VAR}-gcc- \
-  --prefix=/ \
-  --target=${TARGET_VAR}
-
-make -j$(nproc)
-DESTDIR=${ROOTFS}/cross-tools/${TARGET_VAR} make install -j$(nproc)
-
-
-
-
-cd $STAGING
-cd gcc-${GCC_VERSION}
-tar xf ../mpfr.tar.bz2
-mv -v mpfr-3.1.4 mpfr
-tar xf ../gmp.tar.bz2
-mv -v gmp-6.1.1 gmp
-tar xf ../mpc.tar.gz
-mv -v mpc-1.0.3 mpc
-
-mkdir -v build
-cd build
-../configure \
-  --prefix=$ROOTFS/usr \
-  --target=${TARGET_VAR} \
-  --with-sysroot=${ROOTFS}/cross-tools/${TARGET_VAR} \
-  --with-newlib \
-  --enable-initfini-array \
-  --disable-nls \
-  --enable-languages=c,c++ \
-  --enable-c99 \
-  --enable-long-long \
-  --disable-libmudflap \
-  --disable-multilib \
-  --disable-libstdcxx \
-
-make -j$(nproc)
-make install -j$(nproc)
-'
-
-: '
-rm -f $STAGING/config
-touch $STAGING/config
-echo export CC=\""${TARGET_VAR}-gcc --sysroot=${ROOTFS}/targetfs\"" >> $STAGING/config
-echo export CXX=\""${TARGET_VAR}-g++ --sysroot=${ROOTFS}/targetfs\"" >> $STAGING/config
-echo export AR=\""${TARGET_VAR}-ar\"" >> $STAGING/config
-echo export AS=\""${TARGET_VAR}-as\"" >> $STAGING/config
-echo export LD=\""${TARGET_VAR}-ld --sysroot=${ROOTFS}/targetfs\"" >> $STAGING/config
-echo export RANLIB=\""${TARGET_VAR}-ranlib\"" >> $STAGING/config
-echo export READELF=\""${TARGET_VAR}-readelf\"" >> $STAGING/config
-echo export STRIP=\""${TARGET_VAR}-strip\"" >> $STAGING/config
-echo export PATH=\""${ROOTFS}/cross-tools/bin:${PATH}\"" >> $STAGING/config
-source $STAGING/config
-'
-
-# INSTALLING SYSTEM PROGRAMS
-: '
-cp -v ${ROOTFS}/cross-tools/${TARGET_VAR}/lib/libgcc_s.so.1 ${ROOTFS}/lib/
-${TARGET_VAR}-strip ${ROOTFS}/lib/libgcc_s.so.1
-
-cd $STAGING
-cd musl-$MUSL_VERSION
-./configure \
-  CROSS_COMPILE=${TARGET_VAR}- \
-  --prefix=/ \
-  --disable-static \
-  --target=${TARGET_VAR}
-
-make -j$(nproc)
-DESTDIR=$ROOTFS make install-libs -j$(nproc)
-'
+cp memtest.bin $ISO_DIR/boot/memtest
 
 cd $STAGING
 cd busybox-${BUSYBOX_VERSION}
@@ -257,6 +303,7 @@ patch -Np1 -i ../iana-etc-patch.patch
 make get
 make STRIP=yes -j$(nproc)
 make DESTDIR=$ROOTFS install -j$(nproc)
+
 
 cd $ROOTFS
 mkdir -p bin dev mnt proc sys tmp
@@ -310,4 +357,8 @@ EOF
 
 cd $SOURCE_DIR
 grub-mkrescue --compress=xz -o fiordland.iso iso 
+#grub-mkimage -d iso/boot/grub/i386-pc -o iso/boot/grub/i386-pc/core-img -O i386-pc -p iso/boot/grub biosdisk iso9660
+#cat iso/boot/grub/i386-pc/cdboot.img iso/boot/grub/i386-pc/core.img > iso/boot/grub/i386-pc/eltorito.img
+#grub-mkimage -O x86_64-efi -p iso/boot/grub -o iso/efi/boot/bootx64.efi iso9660
+#grub-mkimage -O i386-efi -p iso/boot/grub -o iso/efi/boot/bootia32.efi iso9660
 set +ex
